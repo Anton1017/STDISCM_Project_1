419270b2134ef63383f8824a0a76ed5496fef35d
Anton Louis Moraña <anton_morana@dlsu.edu.ph>
2024-02-07 00:41:52 +0800

    created a window

--- STDISCM_Project_1/STDISCM_Project_1/main.cpp
+++ STDISCM_Project_1/STDISCM_Project_1/main.cpp
@@ -1,43 +1,22 @@
-#include <wx/wx.h>
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
-class MyFrame : public wxFrame
+int main()
 {
-public:
-    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        : wxFrame(NULL, wxID_ANY, title, pos, size)
+    sf::RenderWindow window(sf::VideoMode(1500, 720), "STDISCM_Project_1", sf::Style::Close | sf::Style::Titlebar);
+
+    while (window.isOpen()) 
     {
-        // Create a panel
-        wxPanel* panel = new wxPanel(this, wxID_ANY);
+        sf::Event evnt;
 
-        // Create a button
-        wxButton* button = new wxButton(panel, wxID_ANY, "Click me!", wxPoint(10, 10), wxSize(150, 30));
-
-        // Bind a function to handle button click event
-        button->Bind(wxEVT_BUTTON, &MyFrame::OnButtonClick, this);
+        while (window.pollEvent(evnt)) 
+        {
+            if (evnt.type == evnt.Closed) {
+                window.close();
+            }
+        }
     }
 
-    void OnButtonClick(wxCommandEvent& event)
-    {
-        wxMessageBox("Button clicked!", "Info", wxOK | wxICON_INFORMATION);
-    }
+    return 0;
+}
 
-private:
-    wxDECLARE_EVENT_TABLE();
-};
-
-wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
-EVT_BUTTON(wxID_ANY, MyFrame::OnButtonClick)
-wxEND_EVENT_TABLE()
-
-class MyApp : public wxApp
-{
-public:
-    virtual bool OnInit()
-    {
-        MyFrame* frame = new MyFrame("wxWidgets Sample", wxPoint(50, 50), wxSize(400, 300));
-        frame->Show(true);
-        return true;
-    }
-};
-
-wxIMPLEMENT_APP(MyApp);
